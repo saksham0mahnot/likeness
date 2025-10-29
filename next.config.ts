7ea81610fff479f81e36d7912d7cf294d6d168ef
Saksham Mahnot <117334757+saksham0mahnot@users.noreply.github.com>
2025-10-29 23:11:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "saksham0mahnot.github.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
